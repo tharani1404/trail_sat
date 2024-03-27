f531ab37a4958bfaa4ed47d5ed26b6e8fe5aa5f2
tharani1404 <156342859+tharani1404@users.noreply.github.com>
2024-03-27 20:49:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 from flask import jsonify
 import requests
 from flask import send_from_directory
-from werkzeug.security import safe_str_cmp
+from werkzeug.security import timing_safe_compare
 import cv2
 import moviepy
 from moviepy.editor import VideoFileClip, AudioFileClip,ImageSequenceClip
